c946cae1554b1b042c116d0b4e4af0407f53e59f
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-01-10 17:32:41 +0000

    Added constructor

---
+++ Board.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+
+using namespace std;
+
+// Board has multiple configurations. The configuration is loaded
+// and used to generate a board using this class
+class Board {
+
+  public:
+    Board(string config);
+
+};
