43145134ef9c95a14f6565735b64cc8ed862f018
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-01-10 18:25:22 +0000

    Program sets board/game being played and reports if used together

--- main.cpp
+++ main.cpp
@@ -1,5 +1,48 @@
 #include "main.hpp"
 
-int main(int argc, char** args) {
-  cout << "Starting scrabble bot!..." << endl;
+typedef struct {
+  string board_config;
+  string game_played;
+} options;
+
+static bool errors;
+
+int main(int argc, char** argv) {
+  cout << "Starting scrabble bot!..." << endl << endl;
+
+  options opts;
+  int c;
+
+  while ((c = getopt (argc, argv, "b:g:")) != -1) {
+    switch (c) {
+      case 'b':
+        if (!opts.game_played.empty()) {
+          print_error("You cannot set both the board type and the game you're playing. Using game played as priority.");
+        } else {
+          opts.board_config = optarg;
+        }
+        break;
+      case 'g':
+        if (!opts.board_config.empty()) {
+          print_error("You cannot set both the board type and the game you're playing. Using board config as priority.");
+        } else {
+          opts.game_played = optarg;
+        }
+        break;
+    }
+  }
+
+  cout << opts.board_config << endl;
+  cout << opts.game_played << endl;
+
+  return 0;
+
+}
+
+void print_error(string err) {
+  if (!errors) {
+    cout << "Errors:" << endl;
+    errors = true;
+  }
+  cout << "\t" << err << endl;
 }
--- main.hpp
+++ main.hpp
@@ -4,6 +4,14 @@
 
 #include <iostream>
 
+// As per getopt example
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 using namespace std;
+
+void print_error(string err);
 
 #endif
