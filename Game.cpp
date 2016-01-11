469641cf38a9fa7f6be5a8ed4d156ad8535077c3
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-01-11 16:38:30 +0000

    Partial implementation including opponent go logic

--- Game.cpp
+++ Game.cpp
@@ -10,15 +10,27 @@ Game::Game(options o) : players(o.players), this_player_go(o.this_player_go) {
   rapidjson::Value& scores(d["scores"]);
   this->b = new Board(board_name, width, height, mods, scores);
 
-  assert(o.players > 0 &&
-    o.this_player_go > 0 && o.this_player_go <= o.players
-  );
+  assert(o.players > 0 && o.this_player_go > 0 && o.this_player_go <= o.players);
 
   cout << "Players:\t" << this->players << endl;
   cout << "Your Go:\t" << this->this_player_go << endl;
   cout << endl;
   cout << "Board:\t" << endl;
   this->b->print_board();
+
+  int go = 0;
+  while (!this->is_end()) {
+    int player_turn = go % o.players;
+    if (player_turn == o.this_player_go - 1) {
+      cout << "Your go!" << endl;
+      // this->player_go();
+    } else {
+      cout << "Opponent's go:\tPlayer" << (player_turn + 1) << endl;
+      this->opponent_go();
+      this->b->print_board();
+    }
+    go++;
+  }
 }
 
 rapidjson::Document get_config_from_file(string& config) {
@@ -36,4 +48,116 @@ rapidjson::Document get_config_from_file(string& config) {
   d.Parse(config_file_raw);
 
   return d;
+}
+
+bool Game::is_end(void) {
+  return false;
+}
+
+void Game::opponent_go(void) {
+  while (true) {
+    string input;
+
+    // For putting on the Board
+    string word;
+    int x, y;
+    Direction d;
+
+    while (true) {
+      string word_regex("[A-Za-z]{1,");
+      word_regex.append("2");
+      word_regex.append("}");
+      regex validator(word_regex);
+
+      // Get the word
+      cout << "Please enter the word (if you type more than 1 word, the second will be ignored):\t";
+      cin >> input;
+      cin.clear();
+      cin.ignore(INT_MAX, '\n');
+      if (regex_match(input, validator) && valid_word_for_game(input)) {
+        word = input;
+        break;
+      }
+      cout << "The word you entered was invalid. Please try again." << endl;
+    }
+
+    while (true) {
+      regex y_("^[A-Za-z]$");
+
+      // Get the position
+      cout << "Please enter the position on the board (e.g. 1 A):\t";
+      cin >> input;
+      try {
+        x = stoi(input);
+        if (x <= 0 || x > this->b->get_width()) {
+          throw invalid_argument("");
+        }
+        x -= 1; // Convert from user to program
+      } catch (invalid_argument e) {
+        cout << "The value " << input << " is not in the range {1,"
+             << this->b->get_width() << "}."<< endl;
+        continue;
+      }
+
+      cin >> input;
+      if (!regex_match(input, y_)) {
+        continue;
+      }
+      y = (int) toupper(input[0]) - 'A';
+
+      cin.clear();
+      cin.ignore(INT_MAX, '\n');
+
+      if (valid_position_for_game(x, y)) {
+        break;
+      }
+    }
+
+    while (true) {
+      // Get the word
+      cout << "Please enter the direction (NORTH|EAST|SOUTH|WEST):\t";
+      cin >> input;
+      cin.clear();
+      cin.ignore(INT_MAX, '\n');
+
+      for (auto& c : input) {
+        c = toupper(c);
+      }
+
+      if (input.compare("NORTH") == 0) {
+        d = NORTH; break;
+      } else if (input.compare("EAST") == 0) {
+        d = EAST; break;
+      } else if (input.compare("SOUTH") == 0) {
+        d = SOUTH; break;
+      } else if (input.compare("WEST") == 0) {
+        d = WEST; break;
+      }
+
+      cout << "Incorrect direction. Please try again!" << endl;
+    }
+
+    if (can_put_word_on_board(word, x, y, d)) {
+      this->b->set_word(word, x, y, d);
+      break;
+    }
+
+    cout << "Incorrect parameters specified. Please try again!" << endl;
+  }
+}
+
+void Game::player_go(void) {
+
+}
+
+bool Game::valid_word_for_game(string& input) {
+  return true;
+}
+
+bool Game::valid_position_for_game(int& x, int& y) {
+  return true;
+}
+
+bool Game::can_put_word_on_board(string& word, int& w, int& h, Direction& d) {
+  return true;
 }
--- Game.hpp
+++ Game.hpp
@@ -3,6 +3,9 @@
 #define GAME_H
 
 #include <iostream>
+#include <sstream>
+#include <regex>
+#include <climits>
 
 // Dependencies
 #include "submodules/rapidjson/include/rapidjson/document.h"
@@ -13,12 +16,20 @@
 #include "main.hpp"
 #include "Board.hpp"
 
+using namespace std;
+
 class Game {
 
   private:
     Board* b;
     int players;
     int this_player_go;
+    bool is_end(void);
+    void player_go(void);
+    void opponent_go(void);
+    bool valid_word_for_game(string& input);
+    bool valid_position_for_game(int& x, int& y);
+    bool can_put_word_on_board(string& word, int& w, int& h, Direction& d);
 
   public:
     Game(options o);
